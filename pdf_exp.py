5bfd65efb84ce305b22bf37b4199a4945f579023
hkalsi-usc <60526809+hkalsi-usc@users.noreply.github.com>
2020-04-09 14:06:48 -0700

    Update pdf_exp.py
    Command to run,
    python .\pdf_exp.py <SSTA_Library> <gate_type>
    For Example,
    python .\pdf_exp.py .\tech10nm.sstalib AND

--- pdf_exp.py
+++ pdf_exp.py
@@ -1,36 +1,38 @@
 from PDF import *
+from ckt_sim import PDF_generator,read_sstalib
 import math
 import sys
 
+
 try:
-    gate_type=sys.argv[1]
+    gate_type=sys.argv[2]
     rows=2
     cols=3
-    sstalib = read_sstalib("tech10nm.sstalib")
+    sstalib = read_sstalib(sys.argv[1])
     plt.figure()
     plt.subplot2grid((rows,cols),(0,0),colspan=3)
 
-    ip1 = PDF_generator(sstalib,'IPT',samples,sample_dist)
+    ip1 = PDF_generator(sstalib,'IPT',sample_dist)
     plt.subplot2grid((rows,cols),(0,0))
     plt.title("plot for ip1, mean=%s ,std=%s"%((sstalib['IPT'])['mu'],(sstalib['IPT'])['sigma']))
     plt.xlabel('Delay(ns)')
     plt.ylabel('Probability')
-    sns.scatterplot(ip1.delay, ip1.pdf, color='black')
+    sns.lineplot(ip1.delay, ip1.pdf, color='black')
     
 
-    ip2 = PDF_generator(sstalib,'IPT',samples,sample_dist)
+    ip2 = PDF_generator(sstalib,'IPT',sample_dist)
     plt.subplot2grid((rows,cols),(0,1))
     plt.title("plot for ip2, mean=%s ,std=%s"%((sstalib['IPT'])['mu'],(sstalib['IPT'])['sigma']))
     plt.xlabel('Delay(ns)')
     plt.ylabel('Probability')
-    sns.scatterplot(ip2.delay, ip2.pdf, color='black')
+    sns.lineplot(ip2.delay, ip2.pdf, color='black')
 
-    g1 = PDF_generator(sstalib,gate_type,samples,sample_dist)
+    g1 = PDF_generator(sstalib,gate_type,sample_dist)
     plt.subplot2grid((rows,cols),(0,2))
     plt.title("plot for gate(%s), mean=%s ,std=%s"%(gate_type,(sstalib[gate_type])['mu'],(sstalib[gate_type])['sigma']))
     plt.xlabel('Delay(ns)')
     plt.ylabel('Probability')
-    sns.scatterplot(g1.delay, g1.pdf, color='black')
+    sns.lineplot(g1.delay, g1.pdf, color='black')
     
     moi=ip1.MAX(ip2)
     som = moi.SUM(g1)
@@ -38,7 +40,7 @@ try:
     plt.title("plot for Sum of Max")
     plt.xlabel('Delay(ns)')
     plt.ylabel('Probability')
-    sns.scatterplot(som.delay, som.pdf, color='black', label="Sum of Max")
+    sns.lineplot(som.delay, som.pdf, color='black', label="Sum of Max")
 
     # soi_1 = ip1.SUM(g1)
     # soi_2 = ip2.SUM(g1)
@@ -47,7 +49,7 @@ try:
     # # plt.title("plot for Max of Sum")
     # # plt.xlabel('Delay(ns)')
     # # plt.ylabel('Probability')
-    # sns.scatterplot(mos.delay, mos.pdf, color='red', label="Max of Sum")
+    # sns.lineplot(mos.delay, mos.pdf, color='red', label="Max of Sum")
 
     plt.subplots_adjust(wspace=0.5,hspace=0.7)
     plt.legend(loc="upper left")
